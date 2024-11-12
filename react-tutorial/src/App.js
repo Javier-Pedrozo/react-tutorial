531fcb02ceed053529e96c76eea857d76e30beee
Javier-Pedrozo <javierpedrozomonetta@gmail.com>
2024-11-12 17:49:37 -0300

    requested changes

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,4 +1,4 @@
-import Board from "./Board";
+import Board from "./components/board";
 
 export default function App() {
   return <Board />;
--- react-tutorial/src/components/board/Board.js
+++ react-tutorial/src/components/board/Board.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Square from "./Square";
+import Square from "../square";
 
 function Board() {
   const [xIsNext, setXIsNext] = useState(true);
@@ -17,6 +17,29 @@ function Board() {
     }
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
+  }
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 
   const winner = calculateWinner(squares);
@@ -52,25 +75,4 @@ function Board() {
     </>
   );
 }
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 export default Board;
