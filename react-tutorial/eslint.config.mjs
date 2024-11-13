a09de53f529fe3ebc5c494c890f6916e785a977b
Javier-Pedrozo <javierpedrozomonetta@gmail.com>
2024-11-13 00:32:58 -0300

    basic game (#2)
    * basic game
    
    * requested changes
    
    * requested changes
    
    * requested changes
    
    * names changes temporal
    
    * Correct names

---
+++ react-tutorial/eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
